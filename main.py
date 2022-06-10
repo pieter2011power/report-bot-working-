15889b00818516bb5947936aa3abce265e8c4a79
pieter2011power <61342871+pieter2011power@users.noreply.github.com>
2022-06-10 22:15:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input('import acount link \n')
+print('Hi, %s.' % name)
+for i in range(12342):
+  print(' loading one report:', i)
+name = input('acount succses fully reported \n')
+
+for i in range(12342):
+  print(' loading one report:', i)
+name = input('acount succses fully reported \n')
